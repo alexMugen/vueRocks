e667ed8af37b5a5f3c1a058b1e892f1d76b03a28
Alex <estradealexis09@gmail.com>
2017-10-01 09:10:46 +0200

    move lorem from layout

--- src/components/Conditional.vue
+++ src/components/Conditional.vue
@@ -5,10 +5,10 @@
     <md-layout md-align="center">
       <md-switch v-model="isCardVisible" id="my-test1" name="my-test1" class="md-primary">Show lorem</md-switch>
       <md-switch v-model="isDarkMode" v-if="isCardVisible" id="my-test2" name="my-test2" class="md-primary">Set Dark Mode</md-switch>
-      <div :class="{'dark': isDarkMode}" v-if="isCardVisible">
-        <p class="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium ad consectetur dignissimos dolores enim eveniet fuga harum, odit quasi quidem quos soluta. Assumenda at eaque et id minus? Adipisci.</p>
-      </div>
     </md-layout>
+    <div :class="{'dark': isDarkMode}" v-if="isCardVisible">
+      <p class="md-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium ad consectetur dignissimos dolores enim eveniet fuga harum, odit quasi quidem quos soluta. Assumenda at eaque et id minus? Adipisci.</p>
+    </div>
   </section>
 </template>
 <script>
